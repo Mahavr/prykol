6255aa7527ae45813b075d568344b16aa7370e3a
DeyKast <109087019+DeyKast@users.noreply.github.com>
2023-06-14 10:29:07 +0000

    Deploying to gh-pages from @ Mahavr/prykol@5e1ee22bc56e65d2b088d42d44ef68dfc718c279 🚀

---
+++ index.e679f581.js
@@ -0,0 +1,2 @@
+const e={openModalBtn2:document.querySelector("[data-modal-open2]"),closeModalBtn2:document.querySelector("[data-modal-close2]"),modal2:document.querySelector("[data-modal2]")};(()=>{function o(){e.modal2.classList.toggle("visually-hidden")}e.openModalBtn2.addEventListener("click",o),e.closeModalBtn2.addEventListener("click",o)})();
+//# sourceMappingURL=index.e679f581.js.map
